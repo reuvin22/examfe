e4d573ac597b04d5627bb30ad3d5f7bd3d1eab06
Reuvin Hernandez <reuvinhernandez22@gmail.com>
2024-02-28 10:17:20 +0800

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,151 @@
-import Image from "next/image";
-
+'use client';
+import axios from "axios";
+import { useEffect, useState } from "react";
 export default function Home() {
+  const [data, setData] = useState({
+    author: '',
+    post: ''
+  });
+  const [upData, setUpdate] = useState({
+    id: '',
+    author: '',
+    post: ''
+  });
+  const [updateForm, setForm] = useState(false);
+  const [posts, setPosts] = useState([]);
+  const [trigger, setTrigger] = useState(true);
+
+  const basedUrl = 'http://localhost:8000/api/post';
+  const postData = async() => {
+    try{
+      const response = await axios.post(basedUrl, data, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      console.log(response.data);
+      setTrigger(!trigger);
+      setData({
+        author: '',
+        post: ''
+      });
+    }catch(e){
+      console.error(e);
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    postData();
+  }
+  const handleInput = (e) => {
+    const {name, value} = e.target;
+    setData(prevData => ({
+      ...prevData, [name]:value
+    }));
+  }
+
+  const getAll = async () => {
+    try {
+      const response = await axios.get(basedUrl);
+      setPosts(response.data.data);
+      console.log(response.data.data);
+    } catch (error) {
+      console.error(error.response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    getAll();
+  }, [trigger]);
+
+  const delData = async(id) => {
+    try{
+      const response = await axios.delete(`${basedUrl}/${id}`);
+      alert(response.data.message);
+      setTrigger(!trigger);
+    }catch(e){
+      console.error(e);
+    }
+  }
+  const getData = async(id) => {
+    try{
+      const response = await axios.get(`${basedUrl}/${id}`);
+      setUpdate({
+        id: id,
+        author: response.data.data.author,
+        post: response.data.data.post
+      });
+      setForm(!updateForm);
+    }catch(e){
+      console.log(e);
+    }
+  }
+  const updateData = async () => {
+    try {
+      await axios.put(`${basedUrl}/${upData.id}`, upData, { 
+        headers: { 
+          'Content-Type': 'application/json' 
+        } 
+      });
+      setForm(!updateForm);
+      setTrigger(!trigger);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  const update = e => {
+    e.preventDefault();
+    updateData();
+  }
+  const handleUpdate = e => {
+    const { name, value } = e.target;
+    setUpdate(prevUpdate => ({
+      ...prevUpdate, [name]:value
+    }));
+  }
+  const closeForm = () => {
+    setForm(!updateForm);
+    console.log(updateForm);
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="border-2 w-[100%] h-[100vh] flex justify-center items-center bg-slate-500">
+      <div className="border-2 h-96 w-[50%] sm:w-96 md:w-[80%] bg-blue-500">
+        <form onSubmit={handleSubmit} className="rounded-lg h-20 w-full gap-10 flex justify-center">
+        <input type="text" name="author" className="h-7 w-[30%] ml-5 border-2 mt-5 pl-2" placeholder="Author...." value={data.author} onChange={handleInput}/>
+          <input type="text" name="post" className="h-7 w-[70%] border-2 mt-5 pl-2" placeholder="Post Here...." value={data.post} onChange={handleInput}/>
+          <button type="submit" className="border-2 h-7 mt-5 hover:bg-blue-400 hover:text-white px-10 text-white mr-5">Post</button>
+        </form>
+        <div className={updateForm ? "border-2 h-64 sm:h-64 w-[40%] sm:w-[76%] lg:w-[50%] bg-slate-500 absolute ml-20 sm:ml-0 md:ml-24 md:h-64 lg:ml-0" : "hidden"}>
+          <form onSubmit={update} className="grid gap-5 place-items-center mt-4 w-full">
+            <input type="text" className="h-10 pl-5" name="author" value={upData.author} onChange={handleUpdate}/>
+            <input type="text" className="h-10 pl-5" name="post" value={upData.post} onChange={handleUpdate}/>
+            <button className="border-2 p-2 bg-green-600 text-white w-[48%]">Update</button>
+            <h1 className="border-2 p-2 bg-red-600 text-white cursor-pointer text-center w-[48%]" onClick={closeForm}>Close</h1>
+          </form>
+        </div>
+        <div className="max-h-72 overflow-x-hidden">
+          <table className="ml-5 w-full text-white">
+            <tbody>
+              <tr>
+                <td>Id</td>
+                <td>Author</td>
+                <td>Post</td>
+                <td>Update/Delete</td>
+              </tr>
+              {posts.map(post => (
+                <tr key={post.id}>
+                    <td>{post.id}</td>
+                    <td>{post.author}</td>
+                    <td>{post.post}</td>
+                      <button className="border-2 p-2 bg-green-600" onClick={() => getData(post.id)}>Update</button><button className="border-2 p-2 bg-red-600" onClick={() => delData(post.id)}>Delete</button>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
